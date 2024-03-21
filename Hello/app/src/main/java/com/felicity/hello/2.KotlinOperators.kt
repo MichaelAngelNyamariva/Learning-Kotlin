415eb62c895b1a8f9379f22e6297d6deda4eba66
Tendai Nyamariva <michaelnyamariva@gmail.com>
2024-03-21 18:34:42 +0400

    Update 2.KotlinOperators.kt
    code

--- Hello/app/src/main/java/com/felicity/hello/2.KotlinOperators.kt
+++ Hello/app/src/main/java/com/felicity/hello/2.KotlinOperators.kt
@@ -4,6 +4,14 @@ fun main()
     println("-------------Learning Kotlin Operators---------------");
      //Our Arithmetic operators are / for division, * for multiplication
     // + for addition, - for subtraction  and % modulus
-     var result= 5+3;
-    println("The sum of 5 and 3 is $result")
+     var resultAdd= 5+3;//addition
+    println("The sum of 5 and 3 is $resultAdd")
+     var resultSub=5-3;//subtraction
+    println("The subtraction of 5 and 3 is $resultSub")
+    var resultMult=5*3;//multiplication
+    println("The multiplication of 5 and 3 is $resultMult")
+    var resultDivi:Double=(5.0/3.0);//division
+    println("The division of 5  and 3 is ${resultDivi.toLong()}")
+    var resultModu:Double=(5.0%3.0)
+    println("The modulus of 5  and 3 is $resultModu")
 }
